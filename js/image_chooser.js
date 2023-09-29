13f91319a4134ad243fb3ba8602aa3b830843bc6
Chris <chris.goringe@gmail.com>
2023-09-29 16:51:19 +1000

    clone creates new id, don't randomly change it

--- js/image_chooser.js
+++ js/image_chooser.js
@@ -5,6 +5,12 @@ app.registerExtension({
 	name: "cg.custom.image_chooser",
     async nodeCreated(node) {
         if (node?.comfyClass === "Image Chooser" || node?.comfyClass === "Latent Chooser" ) {
+            const clone = node.clone;
+            node.clone = function () {
+                var cloned = clone.apply(node);
+                cloned.widgets[0].value = Math.floor(Math.random() * 10000000);
+                return cloned;
+            }
             node.widgets[0].value = Math.floor(Math.random() * 10000000);
             const pk_widget = node.addWidget("combo", "choice", 1, () => {}, { values: [1,2,3,4,5,6,7,8] });
             const go_widget = node.addWidget("button", "go", "", (w) => {
@@ -12,7 +18,7 @@ app.registerExtension({
                     const body = new FormData();
                     body.append('selection',w.options.pk.value);
                     body.append('id', node.widgets[0].value);
-                    node.widgets[0].value = Math.floor(Math.random() * 10000000);
+                    //node.widgets[0].value = Math.floor(Math.random() * 10000000);
                     api.fetchApi("/image_selection", { method: "POST", body, });
                 }
             }, {'pk':pk_widget});
@@ -22,7 +28,7 @@ app.registerExtension({
                     const body = new FormData();
                     body.append('selection',0);
                     body.append('id', node.widgets[0].value);
-                    node.widgets[0].value = Math.floor(Math.random() * 10000000);
+                    //node.widgets[0].value = Math.floor(Math.random() * 10000000);
                     api.fetchApi("/image_selection", { method: "POST", body, });
                     api.interrupt();                 
                 }
