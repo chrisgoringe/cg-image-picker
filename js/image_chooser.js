87370af496428db0ab3fb46bca40169eae02e690
Chris <chris.goringe@gmail.com>
2023-09-29 13:23:38 +1000

    randomise id after each click

--- js/image_chooser.js
+++ js/image_chooser.js
@@ -5,27 +5,21 @@ app.registerExtension({
 	name: "cg.custom.image_chooser",
     async nodeCreated(node) {
         if (node?.comfyClass === "Image Chooser" || node?.comfyClass === "Latent Chooser" ) {
-            node.widgets[0].value = Math.floor(Math.random() * 1000000);
+            node.widgets[0].value = Math.floor(Math.random() * 10000000);
             const pk_widget = node.addWidget("combo", "choice", 1, () => {}, { values: [1,2,3,4,5,6,7,8] });
             const go_widget = node.addWidget("button", "go", "", (w) => {
                 const body = new FormData();
                 body.append('selection',w.options.pk.value);
-                if (node.widgets_values) {
-                    body.append('id', node.widgets_values[0]);
-                } else {
-                    body.append('id', node.widgets[0].value);
-                }
+                body.append('id', node.widgets[0].value);
+                node.widgets[0].value = Math.floor(Math.random() * 10000000);
                 api.fetchApi("/image_selection", { method: "POST", body, });
             }, {'pk':pk_widget});
             const cancel_widget = node.addWidget("button", "cancel", "", (w) => {
                 document.getElementById("autoQueueCheckbox").checked = false;
                 const body = new FormData();
                 body.append('selection',w.options.pk.value);
-                if (node.widgets_values) {
-                    body.append('id', node.widgets_values[0]);
-                } else {
-                    body.append('id', node.widgets[0].value);
-                }
+                body.append('id', node.widgets[0].value);
+                node.widgets[0].value = Math.floor(Math.random() * 10000000);
                 api.fetchApi("/image_selection", { method: "POST", body, });
                 api.interrupt();                 
             });
