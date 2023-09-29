119d49bdc4f6630a05392a8aff9e567d82266842
Chris <chris.goringe@gmail.com>
2023-09-29 13:15:06 +1000

    unqiue id for each node

---
+++ js/multipick_widget.js
@@ -0,0 +1,13 @@
+import { app } from "../../../scripts/app.js";
+
+/*app.registerExtension({
+	name: "cg.custom.multipick",
+    async nodeCreated(node) {
+        node?.widgets?.forEach(w => {
+            if (w) {
+                console.log(w);
+            }
+        });
+    }
+});*/
+
