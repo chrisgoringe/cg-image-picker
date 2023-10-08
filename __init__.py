2e002aa28776310e71b8a023b984b2a13af60d57
Chris <chris.goringe@gmail.com>
2023-10-08 20:06:18 +1100

    2.4 clean old js out

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 import sys, os, shutil
+import folder_paths
 
 sys.path.insert(0,os.path.dirname(os.path.realpath(__file__)))
 from .image_chooser import *
-from custom_nodes.cg_custom_core.base import application_web_extensions_directory
 module_root_directory = os.path.dirname(os.path.realpath(__file__))
 module_js_directory = os.path.join(module_root_directory, "js")
 
@@ -15,14 +15,13 @@ NODE_CLASS_MAPPINGS = {
 
 __all__ = ['NODE_CLASS_MAPPINGS']
 
-IP_VERSION = 2.3
-
-shutil.copytree(module_js_directory, os.path.join(application_web_extensions_directory,"..","cg_image_picker"), dirs_exist_ok=True)
+IP_VERSION = 2.4
 
 try:
     from custom_nodes.cg_custom_core import CC_VERSION
     if CC_VERSION < 2.2:
         raise Exception()
+
 except:
     print("cg_custom_core 2.2 not found - will try to install - you may need to restart afterwards")
     from .install import installer
@@ -30,3 +29,11 @@ except:
     import folder_paths
     application_root_directory = os.path.dirname(folder_paths.__file__)
     installer(os.path.join(application_root_directory,"custom_nodes"))
+
+application_root_directory = os.path.dirname(folder_paths.__file__)
+extension_web_extensions_directory = os.path.join(application_root_directory, "web", "extensions", "image_chooser")
+shutil.copytree(module_js_directory, extension_web_extensions_directory, dirs_exist_ok=True)
+
+old_web_extensions_directory = os.path.join(application_root_directory, "web", "extensions", "cg-nodes", "cg_image_picker")
+if os.path.exists(old_web_extensions_directory):
+    shutil.rmtree(old_web_extensions_directory)
