4528f4b7284959d0fe86e34b1cb80eb3206a7e97
Chris <chris.goringe@gmail.com>
2023-10-09 18:24:50 +1100

    x

--- js/image_chooser_preview.js
+++ js/image_chooser_preview.js
@@ -27,16 +27,13 @@ function showImages(node, urls) {
 
 function additionalDrawBackground(node, ctx) {
     node?.selected?.forEach((s) => {
-        var rect = [0, ]
         if (node.imageRects) {
             const rect = node.imageRects[s];
-        }
-        const padding = 8;
-        if (rect) {
+            const padding = 8;
             ctx.strokeStyle = "#8F8";
             ctx.lineWidth = 1;
             ctx.strokeRect(rect[0]+padding, rect[1]+padding, rect[2]-padding*2, rect[3]-padding*2);
-        }
+        } 
     })
 }
 
