7284a835c71d992d5c4863b4916bbf729fe93025
Chris <chris.goringe@gmail.com>
2023-11-15 11:47:32 +1100

    2.6.2 handle list inputs

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ NODE_CLASS_MAPPINGS = {
 
 __all__ = ['NODE_CLASS_MAPPINGS']
 
-IP_VERSION = "2.6.1"
+IP_VERSION = "2.6.2"
 CLEAN = False
 
 application_root_directory = os.path.dirname(folder_paths.__file__)
--- image_chooser_preview.py
+++ image_chooser_preview.py
@@ -9,13 +9,12 @@ class PreviewAndChoose(PreviewImage):
     RETURN_NAMES = ("images","latent",)
     FUNCTION = "func"
     CATEGORY = "image_chooser"
+    INPUT_IS_LIST=True
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                #"id" : ("LABEL", {"value":"__random__", "hidden":True}), 
                 "mode" : (["Always pause", "Only pause if batch"],{}), 
-                
             },
             "optional": {"images": ("IMAGE", ), "latents": ("LATENT", ), },
             "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO", "id":"UNIQUE_ID"},
@@ -24,8 +23,10 @@ class PreviewAndChoose(PreviewImage):
     def IS_CHANGED(self, **kwargs):
         return float('nan')
 
-    def func(self, **kwargs):
-        id = kwargs.pop("id")
+    def func(self, id, **kwargs):
+        # mode doesn't exist in subclass
+        mode = kwargs.pop('mode',["Always pause",])[0]
+        id = id[0]
         if id not in MessageHolder.stash:
             MessageHolder.stash[id] = {}
         my_stash = MessageHolder.stash[id]
@@ -40,16 +41,18 @@ class PreviewAndChoose(PreviewImage):
             
         if (kwargs['images'] is None):
             return (None, None,)
-
-        # extract from inputs
-        latent_in         = kwargs.pop('latents', None)
-        latent_samples_in = latent_in.get('samples',None) if latent_in else None
-        images_in         = kwargs['images']
+        
+        # convert list to batch
+        images_in         = torch.cat(kwargs.pop('images'))
+        latents_in        = kwargs.pop('latents', None)
+        latent_samples_in = torch.cat(list(l['samples'] for l in latents_in)) if latents_in is not None else None
         self.batch        = images_in.shape[0]
-        mode              = kwargs.pop("mode", "Always pause")
 
+        # any other parameters shouldn't be lists any more...
+        for x in kwargs: kwargs[x] = kwargs[x][0]
+ 
         # call PreviewImage base
-        ret = self.save_images(**kwargs)
+        ret = self.save_images(images=images_in, **kwargs)
 
         # send the images to view
         PromptServer.instance.send_sync("early-image-handler", {"id": id, "urls":ret['ui']['images']})
@@ -61,17 +64,17 @@ class PreviewAndChoose(PreviewImage):
             return (None, None,)
         
         return self.batch_up_selections(images_in, latent_samples_in, selections)
-    
-    def tensor_bundle(self, tensor_in, picks):
+
+    def tensor_bundle(self, tensor_in:torch.Tensor, picks):
         return torch.cat(tuple([tensor_in[(x)%self.batch].unsqueeze_(0) for x in picks])).reshape([-1]+list(tensor_in.shape[1:]))
     
-    def latent_bundle(self, latent_samples_in, picks):
+    def latent_bundle(self, latent_samples_in:torch.Tensor, picks):
         if (latent_samples_in is not None and len(picks)):
             return { "samples" : self.tensor_bundle(latent_samples_in, picks) }
         else:
             return None
     
-    def batch_up_selections(self, images_in, latent_samples_in, selections):
+    def batch_up_selections(self, images_in:torch.Tensor, latent_samples_in:torch.Tensor, selections):
         good = [x for x in selections if x>=0]
         images_out = self.tensor_bundle(images_in, good)
         latents_out = self.latent_bundle(latent_samples_in, good)
