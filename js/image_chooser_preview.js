6cc486ff1bcd5d5e1a80234db08dcb782bf34393
rsl8 <138326583+rsl8@users.noreply.github.com>
2024-06-12 20:45:23 +0200

    use relative paths for preview imgs
    fixes #108

--- js/image_chooser_preview.js
+++ js/image_chooser_preview.js
@@ -17,7 +17,7 @@ function showImages(node, urls) {
         const img = new Image();
         node.imgs.push(img);
         img.onload = () => { app.graph.setDirtyCanvas(true); };
-        img.src = `/view?filename=${encodeURIComponent(u.filename)}&type=temp&subfolder=${app.getPreviewFormatParam()}`
+        img.src = `./view?filename=${encodeURIComponent(u.filename)}&type=temp&subfolder=${app.getPreviewFormatParam()}`
     })
     node.setSizeForImage?.();
 }
